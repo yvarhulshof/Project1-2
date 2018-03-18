99aac234fa3ebcfeade2d21e04551fc28d88c073
yvar <yjfhulshof@gmail.com>
2018-03-18 13:40:44 +0100

    acceleration testing and golfball.png added

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -15,12 +15,13 @@ public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
     Rectangle picture;
-    int a;
+    boolean firstPress = true;
+	double startTime = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("golfball3.png");
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
@@ -41,7 +42,17 @@ public class Main extends ApplicationAdapter {
 		batch.end();
 
 
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) picture.x -= 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+		{
+			if(firstPress) startTime = System.nanoTime() / 1000000000.0;
+			firstPress = false;
+
+			picture.x -= 5*Math.pow(System.nanoTime() / 1000000000.0 - startTime,1.2);
+			System.out.println(Gdx.graphics.getDeltaTime() / 100000000);
+
+			//picture.x -= 100*Math.pow(Gdx.graphics.getDeltaTime(),2);
+		}
+
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) picture.x += 200 * Gdx.graphics.getDeltaTime();
 	}
 	
