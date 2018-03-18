a5592afe4609f6023f9c8ac19a3d1e67fcfa7665
yvar <yjfhulshof@gmail.com>
2018-03-18 12:50:47 +0100

    libGDX testing v2

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -2,27 +2,47 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
 
 public class Main extends ApplicationAdapter {
+    OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture img;
+    Rectangle picture;
+    int a;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-	}
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+        picture = new Rectangle();
+        picture.x = 800 / 2 - 64 / 2;
+        picture.y = 20;
+        picture.width = 64;
+        picture.height = 64;
+    }
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0.5f, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, picture.x,picture.y);
 		batch.end();
+
+
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) picture.x -= 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) picture.x += 200 * Gdx.graphics.getDeltaTime();
 	}
 	
 	@Override
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,7 +7,11 @@ import com.mygdx.game.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Test";
+        config.width = 800;
+        config.height = 480;
 		new LwjglApplication(new Main(), config);
 	}
 }
-///
+
+
