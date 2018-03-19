f649c7c1d6adcb7953d74cffb38793c05ed4b8a3
thibtd <35340842+thibtd@users.noreply.github.com>
2018-03-19 18:18:19 +0100

    Add files via upload
    map assets added and map implemented in the main class

---
+++ core/assets/water.tsx
@@ -0,0 +1,4 @@
+
+<tileset name="water" tilewidth="32" tileheight="32" tilecount="40" columns="8">
+ <image source="download.jpg" width="275" height="183"/>
+</tileset>;
--- core/src/com/mygdx/test/Main.java
+++ core/src/com/mygdx/test/Main.java
@@ -3,26 +3,39 @@ package com.mygdx.test;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector3;
 
-public class Main extends ApplicationAdapter{
+public class Main extends ApplicationAdapter implements InputProcessor{
 	OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture img;
 	static Rectangle golfBall;
     PhysicsEngine p;
+	TiledMap tiledMap;
+	TiledMapRenderer tiledMapRenderer;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("golfball3.png");
-
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
+		camera.setToOrtho(false, w, h);
+		camera.update();
+		tiledMap = new TmxMapLoader().load("map.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		Gdx.input.setInputProcessor(this);
 
 		golfBall = new Rectangle();
 		golfBall.x = 800 / 2 - 64 / 2;
@@ -37,6 +50,11 @@ public class Main extends ApplicationAdapter{
 	public void render () {
 		Gdx.gl.glClearColor(0, 0.5f, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(img, golfBall.x,golfBall.y);
 		batch.end();
@@ -51,11 +69,59 @@ public class Main extends ApplicationAdapter{
 		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) golfBall.y -= 200 * Gdx.graphics.getDeltaTime();
 
 	}
+	@Override public boolean keyDown(int keycode) {
+		return false;
+	}
 
-	@Override
+	@Override public boolean keyUp(int keycode) {
+		if(keycode == Input.Keys.LEFT)
+			camera.translate(-32,0);
+		if(keycode == Input.Keys.RIGHT)
+			camera.translate(32,0);
+		if(keycode == Input.Keys.UP)
+			camera.translate(0,32);
+		if(keycode == Input.Keys.DOWN)
+			camera.translate(0,-32);
+		if(keycode == Input.Keys.NUM_1)
+			tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
+		if(keycode == Input.Keys.NUM_2)
+			tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
+		return false;
+	}
+
+	@Override public boolean keyTyped(char character) {
+
+		return false;
+	}
+
+	@Override public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		Vector3 clickCoordinates = new Vector3(screenX,screenY,0);
+		Vector3 position = camera.unproject(clickCoordinates);
+		golfBall.setPosition(position.x, position.y);
+		return true;
+	}
+
+	@Override public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override public boolean scrolled(int amount) {
+		return false;
+	}
+
+
+	/*@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
 	}
-
+ */
 }
