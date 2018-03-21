2a3b3fb525ec15c22ef97a1719e459ee1a83c099
yvar <yjfhulshof@gmail.com>
2018-03-21 23:34:08 +0100

    Cleaned up Main.java and added comments

--- core/src/com/mygdx/test/Main.java
+++ core/src/com/mygdx/test/Main.java
@@ -27,128 +27,98 @@ import java.util.ArrayList;
 import static com.badlogic.gdx.graphics.Color.CYAN;
 
 /**
- * Documentation is essential!!!
+ * Main class which creates the visual representation of the game and updates the game states in its render method
+ * There are 3 implemented methods for moving the ball:
+ * Method 1: Drag and point, a manual way of shooting the ball often used in mini golf video games
+ * Method 2: Velocity input in GUI, using a small GUI the user can input the direction and speed of each swing
+ * Method 3: Reading from a file, using the GolfswingInput.txt file velocity values are read and the ball moves autonomously
  */
 public class Main extends ApplicationAdapter implements InputProcessor{
-	OrthographicCamera camera;
-	SpriteBatch batch;
-	Texture img;
-	static Circle golfBall;
 
-	SpriteBatch goalBatch;
-	Texture goalImg;
-	static Circle goal;
+	private OrthographicCamera camera; //enables us to have a moveable viewpoint (operated by WASD keys)
 
-	SpriteBatch waterBatch;
-	Texture waterImg;
-	static Rectangle water;
+	private SpriteBatch batch; //a collection of image files
+	private Texture golfballImg; //golf ball image file
+	private static Circle golfBall; //golf ball circle object to which the image file is attached
 
-	PhysicsEngine p;
-	TiledMap tiledMap;
-	TiledMapRenderer tiledMapRenderer;
-    static boolean released;
-    SwingInput SI;
-    ArrayList<Rectangle> obstacles;
-    private int numberOfSwings;
-    private int currentSwing;
+	private SpriteBatch goalBatch;
+	private Texture goalImg;
+	private static Circle goal;
+
+	private SpriteBatch waterBatch;
+	private Texture waterImg;
+	private static Rectangle water;
+
+	private PhysicsEngine p;
+	private TiledMap tiledMap;
+	private TiledMapRenderer tiledMapRenderer;
+	private static boolean released;
+	private SwingInput SI; //top left GUI in which swing directions and speed can be entered
+
+    private int numberOfSwings; //used for Method 3, giving the total number of swings entered in the GolswingInput.txt file
+    private int currentSwing; //used for Method 3, giving the current swing number
     private float mouseX;
     private float mouseY;
 
-    private FileInput FI;
-    private String[] mapInfo;
-    private ArrayList<Double> directionValues;
-    private ArrayList<Double> speedValues;
+    private FileInput FI; //instance of FileInput from which GolfswingInput and MapInput can be read
+    private String[] mapInfo; //the course information read from MapInput.txt
+    private ArrayList<Double> directionValues; //the GolfswingInput values
+    private ArrayList<Double> speedValues; //the GolfswingInput values
 
-    private ShapeRenderer sr;
-
+	/**
+	 * Method in which we create the initial game state, load the map, create the file readers and set the physics engine and input processor
+	 */
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("golfball3.png");
+		golfballImg = new Texture("golfball3.png");
 		goalBatch = new SpriteBatch();
 		goalImg = new Texture("circle.png");
 		waterBatch = new SpriteBatch();
 		waterImg = new Texture("water.jpg");
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+		float w = Gdx.graphics.getWidth(); //the width of the screen
+		float h = Gdx.graphics.getHeight(); //the height of the screen
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, w, h);
 		camera.update();
 
-		//tiledMap = new TmxMapLoader().load("map.tmx");
+
 		tiledMap = new TmxMapLoader().load("map2.tmx");
-
-		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-		Gdx.input.setInputProcessor(this);
-		SI = new SwingInput();
-
-		SI.createGUI();
-
-		sr = new ShapeRenderer();
-
-		for(MapObject object : tiledMap.getLayers().get("Obstacles").getObjects()){
-		    Rectangle rect = ((RectangleMapObject) object).getRectangle();
-		    sr.begin(ShapeRenderer.ShapeType.Filled);
-		    sr.rect(rect.x,rect.y,rect.width,rect.height);
-		    sr.end();
-        }
-
-        obstacles = new ArrayList<Rectangle>();
-
-        sr.setColor(CYAN);
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap); //creates the background map (visual)
 
 		water = new Rectangle(1200/2 - 64/2, 70, 120, 80);
-        golfBall = new Circle(800 / 2 - 64 / 2, 20, 25);
-        goal = new Circle(600/2, 40, 30);
+		golfBall = new Circle(800 / 2 - 64 / 2, 20, 25);
+		goal = new Circle(600/2, 40, 30);
 
 
-		double golfBallHeight = Math.sin(golfBall.x) + Math.pow(golfBall.y,2);
+		Gdx.input.setInputProcessor(this); //setting the inputProccesor which allows the user to use the mouse and keyboard to control aspects of the program
 
-		p = new PhysicsEngine(golfBall);
+		SI = new SwingInput(); //creating an instance of the GUI used in Method 2 to enter the velocity of the ball
+		SI.createGUI();
 
-        FI = new FileInput();
 
-	   mapInfo = FI.readMapInfo();
+		p = new PhysicsEngine(golfBall); //creating an instance of the physics engine
 
+
+        FI = new FileInput(); //creating an instance of the file reader
+
+	    mapInfo = FI.readMapInfo(); //receiving information about the map (non-visual, physics related) and then setting these values in the physics engine
         p.setGravitationalForce(Double.parseDouble(mapInfo[0]));
         p.setFrictionConstant(Double.parseDouble(mapInfo[1]));
         p.setMaxSpeed(Double.parseDouble(mapInfo[2]));
 
-        FI.readSwingInfo();
+        FI.readSwingInfo(); //used for Method 3, receiving the swingInput information and then assigning these values
         directionValues = FI.getDirectionValues();
         speedValues = FI.getSpeedValues();
         numberOfSwings = directionValues.size();
         currentSwing = 0;
-
-        for(Double direction : directionValues){
-            System.out.println("dir value: " + direction);
-        }
-
-        for(Double speed : speedValues){
-            System.out.println("speed value: " + speed);
-        }
-
-
-
-
-		Gdx.input.setInputProcessor(this);
 	}
 
 
-	public boolean touchUp(int x, int y, int pointer, int button) {
-		if (button == Input.Buttons.LEFT) {
-			released = true;
-			return true;
-		}
-		return false;
-	}
-	@Override public boolean touchDragged(int screenX, int screenY, int pointer) {
-		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT))
-			return true;
-		else
-			return false;
-	}
-
+	/**
+	 * Method which updates the state of the game and is automatically called 30-60 times per second
+	 * Used to visualize the current game state
+	 */
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0.5f, 0, 0);
@@ -163,19 +133,14 @@ public class Main extends ApplicationAdapter implements InputProcessor{
 		goalBatch.end();
 
 		batch.begin();
-		batch.draw(img, golfBall.x,golfBall.y);
+		batch.draw(golfballImg, golfBall.x,golfBall.y); //draws the golfBall image at the location where the golfBall Circle object is at that point in time
 		batch.end();
 
 		waterBatch.begin();
 		waterBatch.draw(waterImg,water.x,water.y);
 		waterBatch.end();
 
-		sr.setProjectionMatrix(camera.combined);
-
-		//if(i.touchDown(0,0,0,0)) p.moveBall(135,3); //method that moves the ball, starting with initial speed and then deaccelerating
-
-        /*
-		if(pressed)
+		//Method 1 of moving the ball
 		if(released)
 		{
 			p.moveBall(PhysicsEngine.calcAngle(mouseX-golfBall.x, mouseY-golfBall.y), Math.pow(((Math.sqrt(Math.pow((mouseX - golfBall.x), 2) + Math.pow((mouseY - golfBall.y), 2)))/500),2));
@@ -193,54 +158,29 @@ public class Main extends ApplicationAdapter implements InputProcessor{
 			sr.setColor((float) (Math.sqrt(Math.pow((mouseX - golfBall.x), 2) + Math.pow((mouseY - golfBall.y), 2))/300),255 - ((float) (Math.sqrt(Math.pow((mouseX - golfBall.x), 2) + Math.pow((mouseY - golfBall.y), 2))/300)),0,0);
 			sr.line(mouseX, Gdx.graphics.getHeight() - mouseY, golfBall.x + golfBall.radius, golfBall.y + golfBall.radius);
 			sr.end();
-
 		}
 
+
+		//Method 2 of moving the ball
 		if(SI.getButtonClicked())
         {
             p.moveBall(SI.getDir(), SI.getSpd());
         }
 
 
-        for(int i = 0; i < directionValues.size(); i++)
-        {
-            p.moveBall(directionValues.get(i),speedValues.get(i));
-        }
-        */
-
-        //currentSwing = 0;
+		//Method 3 of moving the ball, uncomment and comment Method 1 to use
+		/*
         if(currentSwing < numberOfSwings) p.moveBall(directionValues.get(currentSwing),speedValues.get(currentSwing));
         if(p.ballStopped) currentSwing++;
         if(currentSwing == numberOfSwings) currentSwing = 0;
+		*/
 
-
+		//We update these booleans if the ball is stopped so that we know if we can make another swing
 		if(p.getBallStopped()){
 		    released = false;
             SI.setButtonClicked(false);
         }
 
-        double golfBallEdgeXpos = golfBall.x + golfBall.radius;
-        double golfBallEdgeXneg = golfBall.x - golfBall.radius;
-        double golfBallEdgeYpos = golfBall.y + golfBall.radius;
-        double golfBallEdgeYneg = golfBall.y - golfBall.radius;
-
-        for(Rectangle obst : obstacles){
-		    if(     golfBallEdgeXpos > obst.x && golfBallEdgeXpos < obst.x + obst.width ||
-                    golfBallEdgeXneg > obst.x && golfBallEdgeXneg < obst.x + obst.width ||
-                    golfBallEdgeYpos > obst.x && golfBallEdgeYpos < obst.x + obst.width ||
-                    golfBallEdgeXneg > obst.x && golfBallEdgeYneg < obst.x + obst.width)
-            {
-                System.out.println("collision");
-            }
-            System.out.println("obst x: " + obst.x);
-
-
-		    //if(golfBall.x == obst.x && golfBall.y == obst.y) //should checking all of their coordinates, not just their center
-            //{
-            //    System.out.println("collision");
-            //}
-
-        }
         if(		goal.x - golfBall.x <= 0 && goal.x - golfBall.x >= -80 &&
 				goal.y - golfBall.y <= 0 && goal.y - golfBall.y >= -80){
         	System.out.println("congrats");
@@ -256,8 +196,25 @@ public class Main extends ApplicationAdapter implements InputProcessor{
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) golfBall.x += 200 * Gdx.graphics.getDeltaTime();
 		if(Gdx.input.isKeyPressed(Input.Keys.UP)) golfBall.y += 200 * Gdx.graphics.getDeltaTime();
 		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) golfBall.y -= 200 * Gdx.graphics.getDeltaTime();
-
 	}
+
+
+	//Methods concerning the user controls
+	public boolean touchUp(int x, int y, int pointer, int button) {
+		if (button == Input.Buttons.LEFT) {
+			released = true;
+			return true;
+		}
+		return false;
+	}
+	@Override public boolean touchDragged(int screenX, int screenY, int pointer) {
+		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT))
+			return true;
+		else
+			return false;
+	}
+
+
 	@Override public boolean keyDown(int keycode) {
 		return false;
 	}
@@ -282,19 +239,10 @@ public class Main extends ApplicationAdapter implements InputProcessor{
 
 		return false;
 	}
-/*
-	@Override public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		Vector3 clickCoordinates = new Vector3(screenX,screenY,0);
-		Vector3 position = camera.unproject(clickCoordinates);
-		golfBall.setPosition(position.x, position.y);
-		return true;
-	}
-*/
+
 	@Override public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		return false;
 	}
-
-
 
 	@Override public boolean mouseMoved(int screenX, int screenY) {
 		return false;
@@ -304,11 +252,4 @@ public class Main extends ApplicationAdapter implements InputProcessor{
 		return false;
 	}
 
-/*
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
-*/
 }
