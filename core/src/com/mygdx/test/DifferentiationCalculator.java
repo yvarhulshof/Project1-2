1a68559e0c5b48665defce5033de43dd310bb92b
Aeroique <tanguyaero@gmail.com>
2018-05-28 12:36:16 +0200

    differentiation so far

--- core/src/com/mygdx/test/DifferentiationCalculator.java
+++ core/src/com/mygdx/test/DifferentiationCalculator.java
@@ -9,6 +9,11 @@ public class DifferentiationCalculator {
     double t;
     double y0;
     double h;
+
+    public static void main(String[] args){
+        DifferentiationCalculator dc = new DifferentiationCalculator();
+        dc.calculate();
+    }
 //constructor to get all info from above
     public void calculate(){
         h = 0.5;
@@ -18,51 +23,50 @@ public class DifferentiationCalculator {
         double g = t;
         double count = 0.0;
         coefs.add(w);
+        f = "3x^2+16sinx-54x";
+        //test
+        f(54, 69);
 
-        for(int j = 1; j <= 2; j++){
-            count += h;
-            w = c4rk(h, w, count);
-            coefs.add(w);
-
-        }
-
-        while (count < g-h){
-            count += h;
-            w = AB3SandAM3Smethod(t, h);
-            coefs.add(w);
-        }
-
-
-
-        ArrayList<String> parts = DerivativeCalculator.findCoefErasePlus(equation);
-        System.out.println("-------------parts ");
-        print(parts);
-    }
-
-    public double c4rk(double h, double w, double t){
-        double k1 = h*f(t,w);
-        double k2 = h*f(t + 1/2*h, w + 1/2*k1);
-        double k3 = h*f(t + 1/2*h, w + 1/2*k2);
-        double k4 = h*f(t + h, w + k3);
-
-        double k = 1/6*(k1 + 2*k2 + 2*k3 + k4);
-
-        return w + k;
-    }
-
-    public double AB3SandAM3Smethod(double t, double h){
-
-        double w = coefs.get(coefs.size()-1);
-        double wmin1 = coefs.get(coefs.size()-2);
-        double wmin2 = coefs.get(coefs.size()-3);
-
-        double temp = w + 1/12*h*(23*f(t, w) - 16*f(t-h, wmin1) + 5*f(t-2*h, wmin2));
-
-        return w + 1/24*h*(9*f(t+h, temp) + 19*f(t, w) - 5*f(t-h, wmin1) + f(t-2*h, wmin2));
-
-
+//
+//        for(int j = 1; j <= 2; j++){
+//            count += h;
+//            w = c4rk(h, w, count);
+//            coefs.add(w);
+//
+//        }
+//
+//        while (count < g-h){
+//            count += h;
+//            w = AB3SandAM3Smethod(t, h);
+//            coefs.add(w);
+//        }
 
     }
+
+//    public double c4rk(double h, double w, double t){
+//        double k1 = h*f(t,w);
+//        double k2 = h*f(t + 1/2*h, w + 1/2*k1);
+//        double k3 = h*f(t + 1/2*h, w + 1/2*k2);
+//        double k4 = h*f(t + h, w + k3);
+//
+//        double k = 1/6*(k1 + 2*k2 + 2*k3 + k4);
+//
+//        return w + k;
+//    }
+
+//    public double AB3SandAM3Smethod(double t, double h){
+//
+//        double w = coefs.get(coefs.size()-1);
+//        double wmin1 = coefs.get(coefs.size()-2);
+//        double wmin2 = coefs.get(coefs.size()-3);
+//
+//        double temp = w + 1/12*h*(23*f(t, w) - 16*f(t-h, wmin1) + 5*f(t-2*h, wmin2));
+//
+//        return w + 1/24*h*(9*f(t+h, temp) + 19*f(t, w) - 5*f(t-h, wmin1) + f(t-2*h, wmin2));
+//
+//
+//
+//    }
 
 
 
@@ -74,6 +78,24 @@ public class DifferentiationCalculator {
         print(parts);
 
 
+//        int findPow = DerivativeCalculator.findPow(f);
+//        System.out.println("power: " + findPow);
+
+//        int findcoef = DerivativeCalculator.findOuterCoeff(f);
+//        String findCoefStr = Integer.toString(findcoef);
+//        System.out.println("First coefficient: " + findCoefStr);
+
+        ArrayList<Integer> allNumbers;
+        allNumbers = DerivativeCalculator.findNumbers(f);
+        System.out.println("-------------allNumbers:");
+        for (int i : allNumbers)
+        {
+            System.out.print(" " + i);
+        }
+
+        System.out.println("");
+
+        return 1;
     }
 
 
