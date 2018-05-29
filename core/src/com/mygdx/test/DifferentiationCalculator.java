7c227f4a5effb0d23bd03ec6927bdd73292d3a36
Aeroique <tanguyaero@gmail.com>
2018-05-30 00:12:22 +0200

    differentiation so far

--- core/src/com/mygdx/test/DifferentiationCalculator.java
+++ core/src/com/mygdx/test/DifferentiationCalculator.java
@@ -23,50 +23,51 @@ public class DifferentiationCalculator {
         double g = t;
         double count = 0.0;
         coefs.add(w);
-        f = "cos0";
+        f = "3y+5t";
         //test
-        f(f, 5, 6);
+        //f(f, 5, 6);
 
-//
-//        for(int j = 1; j <= 2; j++){
-//            count += h;
-//            w = c4rk(h, w, count);
-//            coefs.add(w);
-//
-//        }
-//
-//        while (count < g-h){
-//            count += h;
-//            w = AB3SandAM3Smethod(t, h);
-//            coefs.add(w);
-//        }
+
+        for(int j = 1; j <= 2; j++){
+            count += h;
+            w = c4rk(h, w, count);
+            coefs.add(w);
+
+        }
+
+        while (count < g-h){
+            count += h;
+            w = AB3SandAM3Smethod(t, h);
+            coefs.add(w);
+        }
 
     }
 
-//    public double c4rk(double h, double w, double t){
-//        double k1 = h*f(t,w);
-//        double k2 = h*f(t + 1/2*h, w + 1/2*k1);
-//        double k3 = h*f(t + 1/2*h, w + 1/2*k2);
-//        double k4 = h*f(t + h, w + k3);
-//
-//        double k = 1/6*(k1 + 2*k2 + 2*k3 + k4);
-//
-//        return w + k;
-//    }
+    public double c4rk(double h, double w, double t){
+        System.out.println("c4rk here");
+        double k1 = h*f(f,t,w);
+        double k2 = h*f(f,t + 1/2*h, w + 1/2*k1);
+        double k3 = h*f(f,t + 1/2*h, w + 1/2*k2);
+        double k4 = h*f(f,t + h, w + k3);
 
-//    public double AB3SandAM3Smethod(double t, double h){
-//
-//        double w = coefs.get(coefs.size()-1);
-//        double wmin1 = coefs.get(coefs.size()-2);
-//        double wmin2 = coefs.get(coefs.size()-3);
-//
-//        double temp = w + 1/12*h*(23*f(t, w) - 16*f(t-h, wmin1) + 5*f(t-2*h, wmin2));
-//
-//        return w + 1/24*h*(9*f(t+h, temp) + 19*f(t, w) - 5*f(t-h, wmin1) + f(t-2*h, wmin2));
-//
-//
-//
-//    }
+        double k = 1/6*(k1 + 2*k2 + 2*k3 + k4);
+
+        return w + k;
+    }
+
+    public double AB3SandAM3Smethod(double t, double h){
+
+        double w = coefs.get(coefs.size()-1);
+        double wmin1 = coefs.get(coefs.size()-2);
+        double wmin2 = coefs.get(coefs.size()-3);
+
+        double temp = w + 1/12*h*(23*f(f,t, w) - 16*f(f,t-h, wmin1) + 5*f(f,t-2*h, wmin2));
+
+        return w + 1/24*h*(9*f(f,t+h, temp) + 19*f(f,t, w) - 5*f(f,t-h, wmin1) + f(f,t-2*h, wmin2));
+
+
+
+    }
 
 
 
